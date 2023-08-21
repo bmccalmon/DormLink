d56fc367136636e718d6bd284a00da760e400ac0
Ben McCalmon <benmccalmon@gmail.com>
2023-08-21 17:21:55 -0400

    Initial setup

---
+++ installer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""
+This is the installer for applications.
+Provided a remote repository URL, the installer will clone the repository to the "Apps" folder.
+The updater will keep track of the URL and check for updates.
+"""
---
+++ updater.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""
+This is the updater for the DormLink Home screen and all of its applications.
+It uses GitHub to check for updates and clone repositories.
+Any project within the 'Apps' folder will be checked for updates.
+"""
