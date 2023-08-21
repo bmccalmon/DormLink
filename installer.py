bd29320a236f144d6450ac1edb7e72db31473036
Ben McCalmon <benmccalmon@gmail.com>
2023-08-21 19:14:18 -0400

    feat: added an updater

--- installer.py
+++ installer.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 
+import json
+
 """
 This is the installer for applications.
 Provided a remote repository URL, the installer will clone the repository to the "Apps" folder.
-The updater will keep track of the URL and check for updates.
 """
+
+def get_apps_to_download():
+    """
+    This returns a (hopefully) updated list of the endorsed applications available to download.
+    """
+    with open('apps_to_download.json', 'r') as json_file:
+        apps_data = json.load(json_file)
+    return apps_data
+
+def main():
+    print(str(get_apps_to_download()))
+
+if __name__ == "__main__":
+    main()
--- updater.py
+++ updater.py
@@ -1,7 +1,49 @@
 #!/usr/bin/env python3
 
+import sys
+sys.path.append("Dependencies")
+
+import os
+import git
+
 """
 This is the updater for the DormLink Home screen and all of its applications.
 It uses GitHub to check for updates and clone repositories.
 Any project within the 'Apps' folder will be checked for updates.
 """
+
+apps_directory = "Apps"
+
+def update_repo(repository_path):
+    print(f"### Updating {repository_path} ###")
+
+    repo = git.Repo(repository_path)
+    current_branch = repo.active_branch
+    repo.remotes.origin.fetch()
+    remote_branch = repo.remotes.origin.refs[current_branch.name]
+
+    remote_commit_hash = remote_branch.commit.hexsha
+    local_commit_hash = current_branch.commit.hexsha
+
+    if local_commit_hash != remote_commit_hash:
+        print("Update found! Updating...")
+
+    print(f"{repository_path} is up-to-date.")
+
+def update_apps():
+    # Update the main DormLink repository
+    update_main_repository = True
+    if update_main_repository:
+        update_repo('.')
+
+    # Update the installed apps
+    for app in os.listdir(apps_directory):
+        app_path = os.path.join(apps_directory, app)
+        if os.path.isdir(app_path):
+            update_repo(app_path)
+
+def main():
+    update_apps()
+
+if __name__ == "__main__":
+    main()
