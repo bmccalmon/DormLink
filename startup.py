0178771b75d8ad3c87597ed8b7d83141bae8fb0c
Ben McCalmon <benmccalmon@gmail.com>
2023-08-23 17:36:09 -0400

    debug: testing new gui

--- startup.py
+++ startup.py
@@ -4,53 +4,61 @@ import tkinter as tk
 from tkinter import Menu, messagebox
 
 apps_directory = "Apps"
+header_size = 30
+text_size = 21
 
 def main():
     root = tk.Tk()
-    
-    settings_shown = False
 
+    settings_shown = False
     def toggle_settings():
         nonlocal settings_shown
         if settings_shown == False:
-            root.grid_rowconfigure(0, weight=1)
-            root.columnconfigure(0, weight=5)
-            root.columnconfigure(1, weight=1)
-            settings_frame.grid(row=0, column=1, sticky="snew") # sticky="snew"
+            root.grid_columnconfigure(0, weight=1)
+            root.grid_columnconfigure(1, weight=5)
+            settings_label.pack(side="top")
+            update_button.pack(pady=50, fill="x")
             settings_shown = True
         else:
-            settings_frame.grid_forget()
-            root.columnconfigure(1, weight=0)
+            root.columnconfigure(0, weight=1)
+            root.columnconfigure(1, weight=5000)
+            update_button.pack_forget()
+            settings_label.pack_forget()
             settings_shown = False
 
     # Create grid layout with two columns
     root.grid_rowconfigure(0, weight=1)
     root.columnconfigure(0, weight=1)
+    root.columnconfigure(1, weight=5000)
 
-    # Create frames
-    app_list_frame = tk.LabelFrame(root)
-    settings_frame = tk.LabelFrame(root)
+    # Initialize frames
+    apps_frame = tk.LabelFrame(root, bg="white")
+    settings_frame = tk.LabelFrame(root, bg="#d6d6d6")
 
-    # Create buttons
+    # Create app buttons
     app_button_list = list()
     icon_list = list()
     for app in os.listdir(apps_directory):
         icon = tk.PhotoImage(file=f"{apps_directory}/{app}/icon.png")
         icon_list.append(icon)
-        #button = tk.Button(app_list_frame, image=icon, bg="white", relief="flat", command=lambda app=app: print(f"{app} clicked")) # in each app folder, there is an icon.png
-        button = tk.Label(app_list_frame, image=icon)
-        #button.bind("<Button-1>", button_pressed)
+        button = tk.Label(apps_frame, image=icon, bg="white")
         app_button_list.append(button)
 
-    settings = tk.Button(app_list_frame, text="Settings", bg="white", relief="flat", command=toggle_settings)
-    check_for_updates = tk.Button(settings_frame, text="Check for Updates", bg="white", relief="flat")
+    # Settings buttons
+    settings_button = tk.Button(settings_frame, text="", bg="#d6d6d6", relief="flat", borderwidth=0, command=toggle_settings)
+    update_button = tk.Button(settings_frame, text="Check for Updates", bg="white", relief="flat", font=("Arial", text_size))
+
+    # Create labels
+    settings_label = tk.Label(settings_frame, text="Settings", font=("Arial", header_size))
 
     # Lay them out
-    app_list_frame.grid(row=0, column=0, sticky="snew") # sticky="snew"
+    # Apps frame
+    apps_frame.grid(row=0, column=1, sticky="snew")
     for app_button in app_button_list:
         app_button.pack(side="left", padx=20, pady=20, fill="x", expand=True)
-    settings.pack(side="left", padx=20, pady=20, fill="x", expand=True)
-    check_for_updates.pack(expand=True)
+    # Settings frame
+    settings_frame.grid(row=0, column=0, sticky="snew")
+    settings_button.pack(side="right", fill="y")
 
     def set_fullscreen():
         root.attributes("-fullscreen", True)
